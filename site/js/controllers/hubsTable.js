713a695679ffb4403040147c5912732d8da28b73
yintothayang <yintothayang@gmail.com>
2014-08-13 22:10:52 -0700

    I like the direction the web app is going. Step 1. Demonstrate value. As soon as you some how stumble upon juke.parseapp.com you can start using it (whatever ti may do)

---
+++ site/js/controllers/hubsTable.js
@@ -0,0 +1,33 @@
+'use strict';
+
+//Hubs Table Controller
+var HubsTableCtrl = function($scope, $location, ParseService){
+
+    //Get Popular Hubs
+    $scope.getPopularHubs = function(){
+        $scope.$apply(function(){
+            ParseService.getPopularHubs(function(results){
+                $scope.hubs = results;
+            });
+        });
+    },
+
+    //Find Hubs  ***Add Query***
+    $scope.findHubs = function(){
+        $scope.$apply(function(){
+            ParseService.getGetHubs(function(results){
+                $scope.hubs = results;
+            });
+        });
+    },
+
+
+
+    //Init Controller
+    $scope.init = function(){
+        $scope.hubs = {};
+        $scope.getPopularHubs();
+    };
+
+    $scope.init();
+};

48d1b6db39b78db9d52fa6a5ae1ecd63b79625fa
yintothayang <yintothayang@gmail.com>
2015-10-18 13:14:29 -0700

    servire

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash, make_response, send_file, send_from_directory
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def juke():
+    return render_template('index.html')
+
+@app.route('/<path:path>')
+def send_all(path):
+    return send_from_directory('', path)
+
+
+if __name__ == '__main__':
+    app.debug = False
+    app.run(host='0.0.0.0', port=5002)
+

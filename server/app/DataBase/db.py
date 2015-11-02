689a304185b39c213d93ad725e4af380f66c33a9
yintothayang <yintothayang@gmail.com>
2015-11-01 18:21:45 -0800

    moved backend to it's own repo

--- server/app/DataBase/db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
-
-Session = sessionmaker(autocommit=False,
-                       autoflush=False,
-                       bind=create_engine("sqlite:////tmp/test.db"))
-session = scoped_session(Session)
--- server/app/routes.py
+++
@@ -1,5 +0,0 @@
-from app.Resources.hub import *
-from app import *
-
-api.add_resource(HubResource, '/hubs/<id>')
-
